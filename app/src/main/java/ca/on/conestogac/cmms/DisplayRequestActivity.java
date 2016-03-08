6ca31622e2d9f8e34ee75091cc336bf70771e3d8
Pedro <pcamantea@gmail.com>
2016-03-08 17:02:29 -0500

    viewswitcher and layout configuration for different scenarios

--- app/src/main/java/ca/on/conestogac/cmms/DisplayRequestActivity.java
+++ app/src/main/java/ca/on/conestogac/cmms/DisplayRequestActivity.java
@@ -3,14 +3,13 @@ package ca.on.conestogac.cmms;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.text.format.Time;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 import android.widget.ViewSwitcher;
 
@@ -18,10 +17,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.GregorianCalendar;
 
 public class DisplayRequestActivity extends BaseActivity {
     public static final String EXTRA_REQUEST = "ca.on.conestogac.cmms.EXTRA_REQUEST";
@@ -30,7 +27,6 @@ public class DisplayRequestActivity extends BaseActivity {
     public static final String MODE_VIEW = "ViewRequestActivityMODE";
     public static final String MODE_EDIT = "EditRequestActivityMODE";
 
-    private String currentMode;
     private String mDateCreated;
     private String mCreatedBy;
     private String mProgress;
@@ -42,6 +38,8 @@ public class DisplayRequestActivity extends BaseActivity {
     private String mDescription;
     private ArrayAdapter<String> mAdapterRequestFor;
     private WorkRequest workRequest;
+    private String receivedRequest;
+    private String workRequestMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,62 +49,84 @@ public class DisplayRequestActivity extends BaseActivity {
         setSupportActionBar(toolbar);
 
         // for debug
-        String request = getIntent().getStringExtra(EXTRA_REQUEST);
-        String workRequestMode = getIntent().getStringExtra(WORK_REQUEST_MODE);
+        receivedRequest = getIntent().getStringExtra(EXTRA_REQUEST);
+        workRequestMode = getIntent().getStringExtra(WORK_REQUEST_MODE);
 
         initListElements();
 
         // TODO: refactor into specific methods
         try {
-            if (request == null || workRequestMode == null)
-            {
+            if (receivedRequest == null || workRequestMode == null) {
                 Utility.logError("Unexpected call");
                 throw new IllegalArgumentException();
-            }
-            else if (workRequestMode.equals(MODE_EDIT))
-            {
-                // edit mode
-                TextView textViewTest = (TextView) findViewById(R.id.textViewTest);
-                setTitle("Edit Work-Request");
-                currentMode = MODE_EDIT;
-
-                WorkRequest workRequest = new WorkRequest(new JSONObject(request));
-
-                //fillWorkRequestFields(workRequest);
-
+            } else if (workRequestMode.equals(MODE_EDIT)) {
+                workRequest = new WorkRequest(new JSONObject(receivedRequest));
                 Utility.logDebug(workRequest.toString());
-            }
-            else if (workRequestMode.equals(MODE_VIEW))
-            {
-                // view-mode
-                TextView textViewTest = (TextView) findViewById(R.id.textViewTest);
-                setTitle("Work - Request Details");
-                currentMode = MODE_VIEW;
 
+                // edit mode
+                setTitle("Edit Work-Request");
+
+                // Switch do Edits and Spinner
+                SwitchViewsMode(0);
+
+                // Configure buttons visibility
+                Button buttonDisplayRequestCreateMaintenanceLog = (Button) findViewById(R.id.buttonDisplayRequestCreateMaintenanceLog);
+                buttonDisplayRequestCreateMaintenanceLog.setVisibility(View.GONE);
+                Button buttonDisplayRequestEditRequest = (Button) findViewById(R.id.buttonDisplayRequestEditRequest);
+                buttonDisplayRequestEditRequest.setVisibility(View.GONE);
+                Button buttonDisplayRequestCreateRequest = (Button) findViewById(R.id.buttonDisplayRequestCreateRequest);
+                buttonDisplayRequestCreateRequest.setVisibility(View.GONE);
+                Button buttonDisplayRequestSaveEditedRequest = (Button) findViewById(R.id.buttonDisplayRequestSaveEditedRequest);
+                buttonDisplayRequestSaveEditedRequest.setVisibility(View.VISIBLE);
+            } else if (workRequestMode.equals(MODE_VIEW)) {
                 // TODO: setup screen to view mode
-                workRequest = new WorkRequest(new JSONObject(request));
-                ViewSwitcher viewSwitcherRequestID = (ViewSwitcher)findViewById(R.id.viewSwitcherRequestID);
-                viewSwitcherRequestID.setDisplayedChild(1);
-            }
-            else if (workRequestMode.equals(MODE_CREATE))
-            {
+                workRequest = new WorkRequest(new JSONObject(receivedRequest));
+
+                // view-mode
+                setTitle("Work - Request Details");
+
+                // Switch do ViewText
+                SwitchViewsMode(1);
+
+                // Configure buttons visibility
+                Button buttonDisplayRequestCreateMaintenanceLog = (Button) findViewById(R.id.buttonDisplayRequestCreateMaintenanceLog);
+                buttonDisplayRequestCreateMaintenanceLog.setVisibility(View.VISIBLE);
+                Button buttonDisplayRequestEditRequest = (Button) findViewById(R.id.buttonDisplayRequestEditRequest);
+                buttonDisplayRequestEditRequest.setVisibility(View.VISIBLE);
+                Button buttonDisplayRequestCreateRequest = (Button) findViewById(R.id.buttonDisplayRequestCreateRequest);
+                buttonDisplayRequestCreateRequest.setVisibility(View.GONE);
+                Button buttonDisplayRequestSaveEditedRequest = (Button) findViewById(R.id.buttonDisplayRequestSaveEditedRequest);
+                buttonDisplayRequestSaveEditedRequest.setVisibility(View.GONE);
+            } else if (workRequestMode.equals(MODE_CREATE)) {
                 // create
                 // TODO: create JSON for machine ID and use this to create the work request
-                TextView textViewTest = (TextView) findViewById(R.id.textViewTest);
-                setTitle("Create Work-Request");
-                currentMode = MODE_CREATE;
 
+                // Auto-fill date text
                 Calendar calendar = Calendar.getInstance();
-
                 EditText editTextDateCreated = (EditText) findViewById(R.id.editTextDateCreated);
                 editTextDateCreated.setText(Utility.convertDateToStringRaw(calendar.get(Calendar.YEAR),
-                                                                            calendar.get(Calendar.MONTH),
-                                                                            calendar.get(Calendar.DAY_OF_MONTH)));
-                // TODO: hide create maintenance log button
-                LinearLayout linearLayoutRequestID = (LinearLayout)findViewById(R.id.linearLayoutDisplayRequestRequestID);
+                        calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH)));
+
+                setTitle("Create Work-Request");
+
+                // Switch do Edits and Spinner
+                SwitchViewsMode(0);
+
+                // Hide RequestID field since it's a new work request
+                LinearLayout linearLayoutRequestID = (LinearLayout) findViewById(R.id.linearLayoutDisplayRequestRequestID);
                 linearLayoutRequestID.setVisibility(View.GONE);
-            }
-            else {
+
+                // Configure buttons visibility: show button for Save (NEW)
+                Button buttonDisplayRequestCreateMaintenanceLog = (Button) findViewById(R.id.buttonDisplayRequestCreateMaintenanceLog);
+                buttonDisplayRequestCreateMaintenanceLog.setVisibility(View.GONE);
+                Button buttonDisplayRequestEditRequest = (Button) findViewById(R.id.buttonDisplayRequestEditRequest);
+                buttonDisplayRequestEditRequest.setVisibility(View.GONE);
+                Button buttonDisplayRequestCreateRequest = (Button) findViewById(R.id.buttonDisplayRequestCreateRequest);
+                buttonDisplayRequestCreateRequest.setVisibility(View.VISIBLE);
+                Button buttonDisplayRequestSaveEditedRequest = (Button) findViewById(R.id.buttonDisplayRequestSaveEditedRequest);
+                buttonDisplayRequestSaveEditedRequest.setVisibility(View.GONE);
+            } else {
                 // unrecognized mode
                 Utility.logError("unexpected call");
                 throw new IllegalArgumentException();
@@ -116,38 +136,72 @@ public class DisplayRequestActivity extends BaseActivity {
         }
     }
 
+    private void SwitchViewsMode(int viewIndex) {
+        ViewSwitcher viewSwitcherRequestID = (ViewSwitcher) findViewById(R.id.viewSwitcherRequestID);
+        ViewSwitcher viewSwitcherDateCreated = (ViewSwitcher) findViewById(R.id.viewSwitcherDateCreated);
+        ViewSwitcher viewSwitcherCreatedBy = (ViewSwitcher) findViewById(R.id.viewSwitcherCreatedBy);
+        ViewSwitcher viewSwitcherProgress = (ViewSwitcher) findViewById(R.id.viewSwitcherProgress);
+        ViewSwitcher viewSwitcherTitle = (ViewSwitcher) findViewById(R.id.viewSwitcherTitle);
+        ViewSwitcher viewSwitcherRequestFor = (ViewSwitcher) findViewById(R.id.viewSwitcherRequestFor);
+        ViewSwitcher viewSwitcherStatus = (ViewSwitcher) findViewById(R.id.viewSwitcherStatus);
+        ViewSwitcher viewSwitcherPriority = (ViewSwitcher) findViewById(R.id.viewSwitcherPriority);
+        ViewSwitcher viewSwitcherMachineIsRequired = (ViewSwitcher) findViewById(R.id.viewSwitcherMachineIsRequired);
+        viewSwitcherRequestID.setDisplayedChild(viewIndex);
+        viewSwitcherDateCreated.setDisplayedChild(viewIndex);
+        viewSwitcherCreatedBy.setDisplayedChild(viewIndex);
+        viewSwitcherProgress.setDisplayedChild(viewIndex);
+        viewSwitcherTitle.setDisplayedChild(viewIndex);
+        viewSwitcherRequestFor.setDisplayedChild(viewIndex);
+        viewSwitcherStatus.setDisplayedChild(viewIndex);
+        viewSwitcherPriority.setDisplayedChild(viewIndex);
+        viewSwitcherMachineIsRequired.setDisplayedChild(viewIndex);
+    }
+
     private void fillWorkRequestFields(WorkRequest workRequest) {
         // Fill information regardless if it is a TextView or EditText
-        ViewSwitcher viewSwitcherRequestID = (ViewSwitcher)findViewById(R.id.viewSwitcherRequestID);
-        TextView viewRequestID = (TextView)viewSwitcherRequestID.getCurrentView();
+        ViewSwitcher viewSwitcherRequestID = (ViewSwitcher) findViewById(R.id.viewSwitcherRequestID);
+        TextView viewRequestID = (TextView) viewSwitcherRequestID.getCurrentView();
         viewRequestID.setText(workRequest.getRequestID());
 
-        EditText editTextDateCreated = (EditText) findViewById(R.id.editTextDateCreated);
-        editTextDateCreated.setText(workRequest.getDateRequested());
+        ViewSwitcher viewSwitcherDateCreated = (ViewSwitcher) findViewById(R.id.viewSwitcherDateCreated);
+        TextView viewDateCreated = (TextView) viewSwitcherDateCreated.getCurrentView();
+        viewDateCreated.setText(workRequest.getDateRequested());
 
-        EditText editTextCreatedBy = (EditText) findViewById(R.id.editTextCreatedBy);
-        editTextCreatedBy.setText(workRequest.getCreatedBy());
+        ViewSwitcher viewSwitcherCreatedBy = (ViewSwitcher) findViewById(R.id.viewSwitcherCreatedBy);
+        TextView viewCreatedBy = (TextView) viewSwitcherCreatedBy.getCurrentView();
+        viewCreatedBy.setText(workRequest.getCreatedBy());
 
-        EditText editTextProgress = (EditText) findViewById(R.id.editTextProgress);
-        editTextProgress.setText(workRequest.getProgress());
+        ViewSwitcher viewSwitcherProgress = (ViewSwitcher) findViewById(R.id.viewSwitcherProgress);
+        TextView viewProgress = (TextView) viewSwitcherProgress.getCurrentView();
+        viewProgress.setText(workRequest.getProgress());
 
-        EditText editTextTitle = (EditText) findViewById(R.id.editTextTitle);
-        editTextTitle.setText(workRequest.getTitle());
+        ViewSwitcher viewSwitcherTitle = (ViewSwitcher) findViewById(R.id.viewSwitcherTitle);
+        TextView viewTitle = (TextView) viewSwitcherTitle.getCurrentView();
+        viewTitle.setText(workRequest.getTitle());
 
-        int requestForPosition = mAdapterRequestFor.getPosition(workRequest.getRequestFor());
-        if (requestForPosition != -1) {
-            Spinner spinnerDisplayRequestRequestFor = (Spinner) findViewById(R.id.spinnerDisplayRequestRequestFor);
-            spinnerDisplayRequestRequestFor.setSelection(requestForPosition);
+        if (!workRequestMode.equals(MODE_VIEW)) {
+            int requestForPosition = mAdapterRequestFor.getPosition(workRequest.getRequestFor());
+            if (requestForPosition != -1) {
+                Spinner spinnerDisplayRequestRequestFor = (Spinner) findViewById(R.id.spinnerDisplayRequestRequestFor);
+                spinnerDisplayRequestRequestFor.setSelection(requestForPosition);
+            }
+        }
+        else {
+            TextView textViewRequestFor = (TextView) findViewById(R.id.textViewRequestFor);
+            textViewRequestFor.setText(workRequest.getRequestFor());
         }
 
-        EditText editTextStatus = (EditText) findViewById(R.id.editTextStatus);
-        editTextStatus.setText(workRequest.getStatus());
+        ViewSwitcher viewSwitcherStatus = (ViewSwitcher) findViewById(R.id.viewSwitcherStatus);
+        TextView viewStatus = (TextView) viewSwitcherStatus.getCurrentView();
+        viewStatus.setText(workRequest.getStatus());
 
-        EditText editTextPriority = (EditText) findViewById(R.id.editTextPriority);
-        editTextPriority.setText(workRequest.getPriority());
+        ViewSwitcher viewSwitcherPriority = (ViewSwitcher) findViewById(R.id.viewSwitcherPriority);
+        TextView viewPriority = (TextView) viewSwitcherPriority.getCurrentView();
+        viewPriority.setText(workRequest.getPriority());
 
-        EditText editTextMachineIsRequired = (EditText) findViewById(R.id.editTextMachineIsRequired);
-        editTextMachineIsRequired.setText(workRequest.getDateDue());
+        ViewSwitcher viewSwitcherMachineIsRequired = (ViewSwitcher) findViewById(R.id.viewSwitcherMachineIsRequired);
+        TextView viewMachineIsRequired = (TextView) viewSwitcherMachineIsRequired.getCurrentView();
+        viewMachineIsRequired.setText(workRequest.getDateDue());
 
         EditText editTextDescriptionOfRequest = (EditText) findViewById(R.id.editTextDescriptionOfRequest);
         editTextDescriptionOfRequest.setText(workRequest.getDescription());
@@ -190,8 +244,9 @@ public class DisplayRequestActivity extends BaseActivity {
         mAdapterRequestFor = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
         mAdapterRequestFor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        Spinner spinnerRequestFor = (Spinner)findViewById(R.id.spinnerDisplayRequestRequestFor);
+        Spinner spinnerRequestFor = (Spinner) findViewById(R.id.spinnerDisplayRequestRequestFor);
         spinnerRequestFor.setAdapter(mAdapterRequestFor);
+
         spinnerRequestFor.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view,
@@ -214,19 +269,19 @@ public class DisplayRequestActivity extends BaseActivity {
         try {
             JSONObject jsonObject = new JSONObject(jsonString);
             String result = jsonObject.getString("result");
-            if (result.compareTo(ValueConstants.RET_OK) != 0 ) {
+            if (result.compareTo(ValueConstants.RET_OK) != 0) {
                 // do something if needed when error happens
             }
-            if(jsonObject.has("createdRequestID")) {
+            if (jsonObject.has("createdRequestID")) {
                 String createdRequestID = jsonObject.getString("createdRequestID");
                 Utility.showToast(this, "Request mock created with ID: " + createdRequestID);
                 // TODO: redirect to machine information?
             }
 
-            if(jsonObject.has("requestForList")) {
+            if (jsonObject.has("requestForList")) {
                 fillRequestForSpinner(jsonObject);
 
-                if (currentMode != MODE_CREATE) {
+                if (!workRequestMode.equals(MODE_CREATE)) {
                     fillWorkRequestFields(workRequest);
                 }
             }
@@ -257,6 +312,7 @@ public class DisplayRequestActivity extends BaseActivity {
 
     public void onClickCreateMaintenanceLog(View view) {
         Intent intent = new Intent(this, DisplayMaintenanceLogActivity.class);
+        // TODO: send work request as putextra
         startActivity(intent);
     }
 
@@ -264,7 +320,7 @@ public class DisplayRequestActivity extends BaseActivity {
         getWorkRequestFields();
         JSONObject jsonParam = new JSONObject();
 
-        try{
+        try {
             jsonParam.put("userID", User.getInstance().userID);
             // TODO fill machine ID
             jsonParam.put("machineID", "123");
@@ -284,5 +340,13 @@ public class DisplayRequestActivity extends BaseActivity {
         }
 
         callAPI("CreateWorkRequest", jsonParam);
+    }
+
+    public void onClickCreateRequestActivitySaveEditedRequest(View view) {
+
+    }
+
+    public void onClickCreateRequestActivityBeginEditRequest(View view) {
+
     }
 }

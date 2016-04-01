b827a49d8af8cc81b19badd43ce72c1a1725c743
mikezindel <mikezindel@hotmail.com>
2016-04-01 13:43:59 -0400

    crashing code

--- app/src/main/java/ca/on/conestogac/cmms/MaintenanceAdapter.java
+++ app/src/main/java/ca/on/conestogac/cmms/MaintenanceAdapter.java
@@ -25,13 +25,17 @@ public class MaintenanceAdapter extends ArrayAdapter<MaintenanceLog> {
     public View getView(int position, View convertView, ViewGroup parent) {
         convertView = layoutInflater.inflate(R.layout.item_maintenance_log, parent, false);
         ((TextView)convertView.findViewById(R.id.textViewMaintenanceLogDate)).setText("Date: " + Utility.convertDateYYYYMMDDToShow(getItem(position).getDate()));
-        ((TextView)convertView.findViewById(R.id.textViewMaintenanceLogName)).setText("Completed By: " + (getItem(position).getContractor()));
-        ((TextView)convertView.findViewById(R.id.textViewMaintenanceLogMaintenanceRequired)).setText(getItem(position).getMaintenanceRequired());
-        ((TextView)convertView.findViewById(R.id.textViewMaintenanceLogMachineID)).setText(getItem(position).getMachineID());
-        ((TextView)convertView.findViewById(R.id.textViewMaintenanceLogRequestID)).setText(getItem(position).getRequestID());
-        ((TextView)convertView.findViewById(R.id.textViewMaintenanceLogMaintenanceID)).setText(getItem(position).getMaintenanceLogID());
-        ((TextView)convertView.findViewById(R.id.textViewMaintenanceLogActionTaken)).setText(getItem(position).getActionTaken());
-
+        ((TextView)convertView.findViewById(R.id.textViewMaintenanceLogName)).setText("Completed by: " + (getItem(position).getCompletedBy()));
+        ((TextView)convertView.findViewById(R.id.textViewMaintenanceLogMaintenanceRequired)).setText("Maintenance required: " + (getItem(position).getMaintenanceRequired()));
+        ((TextView)convertView.findViewById(R.id.textViewMaintenanceLogContractor)).setText("Contractor: " + (getItem(position).getContractor()));
+        ((TextView)convertView.findViewById(R.id.textViewMaintenanceLogContractorCompany)).setText("Contractor company: " + (getItem(position).getContractorCompany()));
+        ((TextView)convertView.findViewById(R.id.textViewMaintenanceLogPartsRequired)).setText("Parts required: " + (getItem(position).getPartsRequired()));
+        ((TextView)convertView.findViewById(R.id.textViewMaintenanceLogActionTaken)).setText("Action taken: " + (getItem(position).getActionTaken()));
+        ((TextView)convertView.findViewById(R.id.textViewMaintenanceLogPartCost)).setText("Part cost: " + (getItem(position).getPartCost()));
+        ((TextView)convertView.findViewById(R.id.textViewMaintenanceLogMaintenanceID)).setText("MaintenanceID: " + (getItem(position).getMaintenanceLogID()));
+        ((TextView)convertView.findViewById(R.id.textViewMaintenanceLogMachineID)).setText("MachineID: " + (getItem(position).getMachineID()));
+        ((TextView)convertView.findViewById(R.id.textViewMaintenanceLogRequestID)).setText("RequestID: " + (getItem(position).getRequestID()));
+        ((TextView)convertView.findViewById(R.id.textViewMaintenanceLogMaintenanceLogID)).setText("Maintenance Log ID: " + (getItem(position).getMaintenanceLogID()));
         return convertView;
     }
 }
